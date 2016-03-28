2e6d66c8e9c574cb149e79c627c6754f7d02502f
kevorkian <jralfarog@gmail.com>
2016-03-28 01:04:06 -0600

    Filtros basicos actualizado

--- js/app.js
+++ js/app.js
@@ -536,3 +536,35 @@ nameApp.directive('goNative', ['$ionicGesture', '$ionicPlatform', function($ioni
                 }
             };
         }]);
+
+
+  nameApp.controller('MyController', function($scope, $ionicModal) {
+  $scope.modalClasses = ['slide-in-up', 'slide-in-down', 'fade-in-scale', 'fade-in-right', 'fade-in-left', 'newspaper', 'jelly', 'road-runner', 'splat', 'spin', 'swoosh', 'fold-unfold'];
+
+  $scope.openModal = function(animation) {
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+      scope: $scope,
+      animation: animation
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+     $scope.modal.remove();
+
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+});
